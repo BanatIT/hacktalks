3993b57ab084038a2a32ba8f1e7983b82a9f1c74
silvia <silvia@silvias-MacBook.local>
2018-10-11 17:59:14 +0300

    what goppo said

--- index.php
+++ index.php
@@ -782,7 +782,7 @@ My unique blend of front-end and back-end skills coupled with my hard work ethic
                 <div class="modal-dialog modal-dialog-centered" role="document">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h5 class="modal-title" >Marco Rigamonti</h5>
+                      <h5 class="modal-title" >Rod Smith</h5>
                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                       </button>
